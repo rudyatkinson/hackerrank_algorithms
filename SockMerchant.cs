c35c1654f6ac037a3b429650313a7639116da98c
Muhammed Bilal Aydın <54232905+rudyatkinson@users.noreply.github.com>
2020-06-27 16:33:25 +0300

    Create SockMerchant.cs

---
+++ SockMerchant.cs
@@ -0,0 +1,50 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+
+        Dictionary<int, int> dict = new Dictionary<int, int>(); 
+        foreach(int i in ar){
+
+            if(!dict.ContainsKey(i))
+                dict.Add(i, 1);
+            else
+                dict[i]++;
+        }
+
+        int pairs = 0;
+        foreach(KeyValuePair<int, int> i in dict)
+            pairs += i.Value / 2;
+
+        return pairs;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+        ;
+        int result = sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
