42514335adac64cb7c4c9c1defd2893ca0f9937a
Muhammed Bilal Aydın <54232905+rudyatkinson@users.noreply.github.com>
2020-07-04 22:12:00 +0300

    Create JumpingOnTheClouds.cs

---
+++ JumpingOnTheClouds.cs
@@ -0,0 +1,53 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the jumpingOnClouds function below.
+    static int jumpingOnClouds(int[] c, int k) {
+        int currentPosition = 0;
+        int health = 100;
+        while(true)
+        {
+            int newPosition = (currentPosition + k) % c.Count();
+            if(currentPosition == 0 && health != 100)
+                break;
+            else
+            {
+                health -= c[currentPosition] == 0 ? 1 : 3;
+                currentPosition = newPosition;
+            }   
+        }
+        return health;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] nk = Console.ReadLine().Split(' ');
+
+        int n = Convert.ToInt32(nk[0]);
+
+        int k = Convert.ToInt32(nk[1]);
+
+        int[] c = Array.ConvertAll(Console.ReadLine().Split(' '), cTemp => Convert.ToInt32(cTemp))
+        ;
+        int result = jumpingOnClouds(c, k);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
